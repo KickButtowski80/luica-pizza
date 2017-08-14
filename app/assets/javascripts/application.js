b655e2eb6b2440877d059a2592ae05966f7d8b82
kickbuttowski59 <pazpaz22@yahoo.com>
2017-08-14 17:45:51 +0000

    lol

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
+//= require turbolinks
 //= require jquery3
 //= require popper
-
+//= require bootstrap-sprockets
 //= require_tree .
