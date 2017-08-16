4d2498f9ed2137268cd2ed6f041f21685ee14230
kickbuttowski59 <pazpaz22@yahoo.com>
2017-08-16 22:28:58 +0000

    added config.assets.paths << "#{Rails.root}/public/videos"

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Workspace
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     #config.serve_static_assets = true 
+    config.assets.paths << "#{Rails.root}/public/videos"
     module YourApp
       class Application < Rails::Application
         config.middleware.use Rack::Deflater
