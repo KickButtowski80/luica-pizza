02e12a970e58d54c4992fdfddb11c3a124a131b4
kickbuttowski59 <pazpaz22@yahoo.com>
2017-08-16 20:38:52 +0000

    added Passive event listeners to scrolling

--- app/assets/javascripts/smoothscroll.js
+++ app/assets/javascripts/smoothscroll.js
@@ -1,15 +1,17 @@
 $(document).ready( function() {
-   var $root = $('html, body');
-    $('.scroll').click( function(e) {
-         var target = this.hash;
-         var $target = $(target);
-         if($target.length > 0){
-             e.preventDefault(); 
-             $("html,body").animate({
-                 'scrollTop': $target.offset().top
-             }, 1000, 'swing', function(){
-                 window.location.hash = target;
-             });
-         }
-    });
+    window.addEventListener('click', function(event) {
+       var $root = $('html, body');
+        $('.scroll').click( function(e) {
+             var target = this.hash;
+             var $target = $(target);
+             if($target.length > 0){
+                 e.preventDefault(); 
+                 $("html,body").animate({
+                     'scrollTop': $target.offset().top
+                 }, 1000, 'swing', function(){
+                     window.location.hash = target;
+                 });
+             }
+        });
+    }, {passive: true});
  });
