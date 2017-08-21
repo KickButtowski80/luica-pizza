9a49224a557598be44fa66d08448c245c4a39ede
kickbuttowski59 <pazpaz22@yahoo.com>
2017-08-21 19:30:28 +0000

    better zoom size and better src for the google map

--- app/assets/javascripts/load-google-map.js
+++ app/assets/javascripts/load-google-map.js
@@ -5,10 +5,10 @@ $(function() {
             lat: 34.027048,
             lon: -118.391561,
             title:'Lucia Pizza',
-            html: ['<h3>Lucia Pizza</h3>',
+            html: ['<h5>Lucia Pizza</h5>',
                    '<p>9030 Venice Blvd Los Angeles CA, 90232</p>',
                    '<p> tel: (310) 838-0331 </p>'].join(''),
-            zoom: 10
+            zoom: 19
         }]
     }).Load();
 });
