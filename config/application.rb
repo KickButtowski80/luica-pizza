f12a7f90b192da6e91e654d8b2fd12fccabdf3e3
kickbuttowski59 <pazpaz22@yahoo.com>
2017-08-22 23:47:35 +0000

    no rack deflater with use, instead use rack sendfile

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ Bundler.require(*Rails.groups)
  
 module Workspace
   class Application < Rails::Application
+    
+    config.middleware.insert_before(Rack::Sendfile, Rack::Deflater) 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -18,12 +20,12 @@ module Workspace
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+  
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     #config.serve_static_assets = true 
     config.assets.paths << "#{Rails.root}/public/videos"
-    config.middleware.use Rack::Deflater
+    #config.middleware.use Rack::Deflater
   
   end
 end
