c29b557b69fed02121129f9abf6a0caca7c46404
kickbuttowski59 <pazpaz22@yahoo.com>
2017-08-22 22:44:35 +0000

    used rake deflater with use

--- config/application.rb
+++ config/application.rb
@@ -23,10 +23,7 @@ module Workspace
     config.active_record.raise_in_transactional_callbacks = true
     #config.serve_static_assets = true 
     config.assets.paths << "#{Rails.root}/public/videos"
-    # module YourApp
-    #   class Application < Rails::Application
-    #     config.middleware.use Rack::Deflater
-    #   end
-    # end
+    config.middleware.use Rack::Deflater
+  
   end
 end
