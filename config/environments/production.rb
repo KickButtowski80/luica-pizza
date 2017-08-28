cc3195cc48212cbd713cc9fc4580a38527fda43f
kickbuttowski59 <pazpaz22@yahoo.com>
2017-08-28 20:58:16 +0000

    found the name of app Rails.application.class.to_s.split("::").first"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
   config.cache_store = :dalli_store, 'cache-1.example.com', 'cache-2.example.com:11211:2',
-  { :namespace => tout-de-cake-menu, :expires_in => 1.day, :compress => true }
+  { :namespace => Workspace, :expires_in => 1.day, :compress => true }
   
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
