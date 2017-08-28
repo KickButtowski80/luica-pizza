7f423378b22369eb3756d6bf6619acb05f73f0e8
kickbuttowski59 <pazpaz22@yahoo.com>
2017-08-28 17:32:40 +0000

    makes smooth scrolling works smoother

--- app/assets/javascripts/smoothscroll.js
+++ app/assets/javascripts/smoothscroll.js
@@ -1,13 +1,12 @@
 // https://en.wikipedia.org/wiki/Immediately-invoked_function_expression
 // https://css-tricks.com/snippets/jquery/smooth-scrolling/
 $(document).ready(function(){
-    window.addEventListener('click', function(event) {
        var $root = $('html, body');
         $('.scroll').click( function(e) {
              var target = this.hash;
              var $target = $(target);
              if($target.length > 0){
-                 e.preventDefault();
+                 //e.preventDefault();
                  $("html,body").animate({
                      'scrollTop': $target.offset().top
                  }, 1000, 'swing', function(){
@@ -15,5 +14,4 @@ $(document).ready(function(){
                  });
              }
         });
-    }, { passive: true });
  });
