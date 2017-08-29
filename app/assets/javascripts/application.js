101813919a1079566fe3d4e401198ba918d6886d
kickbuttowski59 <pazpaz22@yahoo.com>
2017-08-29 21:56:19 +0000

    now just have jquery3 in application.js and trubolink works fine

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require jquery3
+//= require jquery_ujs 
 //= require lib/typed
 //= require typed-implementation
-//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
 
--- app/assets/javascripts/smoothscroll.js
+++ app/assets/javascripts/smoothscroll.js
@@ -1,12 +1,12 @@
 // https://en.wikipedia.org/wiki/Immediately-invoked_function_expression
 // https://css-tricks.com/snippets/jquery/smooth-scrolling/
-$(document).ready(function(){
+$(document).on("turbolinks:load",function(){
        var $root = $('html, body');
         $('.scroll').click( function(e) {
              var target = this.hash;
              var $target = $(target);
              if($target.length > 0){
-                 //e.preventDefault();
+                 e.preventDefault();
                  $("html,body").animate({
                      'scrollTop': $target.offset().top
                  }, 1000, 'swing', function(){
