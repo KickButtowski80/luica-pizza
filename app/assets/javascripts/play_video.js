4dc623fe8c4ad89329e2e436ec4121a2334ddebc
kickbuttowski59 <pazpaz22@yahoo.com>
2017-08-30 18:36:41 +0000

    added trubolinks load to all js file excpet main and Maplace js files

--- app/assets/javascripts/play_video.js
+++ app/assets/javascripts/play_video.js
@@ -1,4 +1,4 @@
-$(document).ready( function() {
+$(document).on('turbolinks:load', function(){
     var video = document.getElementById('vid');
     video.addEventListener('onload',function(){
       video.play();
